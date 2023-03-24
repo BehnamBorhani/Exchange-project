7e19ed68368767b5336e5920a0fc60a2a559815a
Behnam <behnamborhani95@gmail.com>
2023-03-24 20:12:50 +0330

    build: theme switch description: change light and dark mod theme

--- js/script.js
+++ js/script.js
@@ -13,12 +13,36 @@ const logoImg = document.querySelector(".logo img");
 document.addEventListener("scroll", function () {
    if (document.documentElement.scrollTop > 0) {
       logoImg.style.height = "30px";
-      navbar.classList.add("bg-black");
-      navbar.classList.add("txt-white");
+      /* navbar.classList.add("bg-black");
+      navbar.classList.add("txt-white"); */
    } else {
       logoImg.style.height = "30px";
-      navbar.classList.remove("bg-black");
-      navbar.classList.remove("txt-white");
+      /* navbar.classList.remove("bg-black");
+      navbar.classList.remove("txt-white"); */
    }
 });
 /* navbar ends */
+
+/* dark mode starts */
+const switchElement = document.querySelector('.switch')
+
+switchElement.addEventListener('click', function () {
+  document.body.classList.toggle('dark')
+
+  if (document.body.className.includes('dark')) {
+    localStorage.setItem('theme', 'dark')
+  } else {
+    localStorage.setItem('theme', 'light')
+  }
+})
+
+
+window.onload = function () {
+  let localStorageTheme = localStorage.getItem('theme')
+
+  if (localStorageTheme === 'dark') {
+    document.body.classList.add('dark')
+  }
+
+}
+/* dark mode ends */
