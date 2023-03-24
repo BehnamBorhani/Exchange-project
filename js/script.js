ea785744f808da3108a2b2c1dcc161ca659d0870
Behnam <behnamborhani95@gmail.com>
2023-03-24 17:22:05 +0330

    build: navbar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ window.addEventListener("load", function () {
    preloaderElem.classList.add("hidden");
 });
 /* preloader ends */
+
+/* navbar starts */
+const navbar = document.querySelector("#navbar");
+const logoImg = document.querySelector(".logo img");
+
+document.addEventListener("scroll", function () {
+   if (document.documentElement.scrollTop > 0) {
+      logoImg.style.height = "30px";
+      navbar.classList.add("bg-black");
+      navbar.classList.add("txt-white");
+   } else {
+      logoImg.style.height = "30px";
+      navbar.classList.remove("bg-black");
+      navbar.classList.remove("txt-white");
+   }
+});
+/* navbar ends */
