33e0e0fbc1e9018220971cacfa1ac8eb4aa00524
Behnam <behnamborhani95@gmail.com>
2023-03-24 22:48:11 +0330

    build: custom scrollbar

--- js/script.js
+++ js/script.js
@@ -9,8 +9,16 @@ window.addEventListener("load", function () {
 /* navbar starts */
 const navbar = document.querySelector("#navbar");
 const logoImg = document.querySelector(".logo img");
+let customScroll = document.querySelector("#scroll");
 
 document.addEventListener("scroll", function () {
+   let scrollTop = window.scrollY;
+   let documentHeight = document.body.clientHeight;
+   let windowHeight = window.innerHeight;
+   let scrollPercent = scrollTop / (documentHeight - windowHeight);
+   let scrollPercentRounded = Math.round(scrollPercent * 100);
+   customScroll.style.width = scrollPercentRounded + "%";
+
    if (document.documentElement.scrollTop > 0) {
       logoImg.style.height = "30px";
       /* navbar.classList.add("bg-black");
@@ -24,25 +32,23 @@ document.addEventListener("scroll", function () {
 /* navbar ends */
 
 /* dark mode starts */
-const switchElement = document.querySelector('.switch')
+const switchElement = document.querySelector(".switch");
 
-switchElement.addEventListener('click', function () {
-  document.body.classList.toggle('dark')
+switchElement.addEventListener("click", function () {
+   document.body.classList.toggle("dark");
 
-  if (document.body.className.includes('dark')) {
-    localStorage.setItem('theme', 'dark')
-  } else {
-    localStorage.setItem('theme', 'light')
-  }
-})
-
+   if (document.body.className.includes("dark")) {
+      localStorage.setItem("theme", "dark");
+   } else {
+      localStorage.setItem("theme", "light");
+   }
+});
 
 window.onload = function () {
-  let localStorageTheme = localStorage.getItem('theme')
+   let localStorageTheme = localStorage.getItem("theme");
 
-  if (localStorageTheme === 'dark') {
-    document.body.classList.add('dark')
-  }
-
-}
+   if (localStorageTheme === "dark") {
+      document.body.classList.add("dark");
+   }
+};
 /* dark mode ends */
