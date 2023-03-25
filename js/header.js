3781b5fef5c1da568dec9cf71551bdc14f37d441
Behnam <behnamborhani95@gmail.com>
2023-03-25 12:30:04 +0330

    build: Dynamic title

---
+++ js/header.js
@@ -0,0 +1,20 @@
+const highlightElem = document.querySelector("#highlight");
+const words = [
+   { word: "Blockchain", color: "#03ca9b" },
+   { word: "Token", color: "#A06EEB" },
+   { word: "Dex", color: "#ef14c3" },
+   { word: "AMM", color: "#FC7F44" },
+];
+
+window.addEventListener("load", () => {
+   let index = 0;
+   setInterval(() => {
+      highlightElem.innerText = words[index].word;
+      highlightElem.style.color = words[index].color;
+      index++;
+
+      if (index >= words.length) {
+         index = 0;
+      }
+   }, 2000);
+});
