5c4321ec2a397760fa199cba2d0e8541c79f35c2
Behnam <behnamborhani95@gmail.com>
2023-04-15 18:08:25 +0330

    aggregate all logo

--- js/api.js
+++ js/api.js
@@ -1,9 +1,4 @@
 /* market overview starts */
-/* fetch("http://rest.coinapi.io/v1/assets/icons/32?apikey=08F3391E-4647-41E2-A39B-7884EE0EE74D")
-   .then((res) => res.json())
-   .then((res) => console.log(res)); */
-/* market overview ends */
-
 async function fetchData() {
    let tableElem = document.querySelector("table tbody");
    let allCoins = await fetch(
@@ -42,3 +37,4 @@ async function fetchData() {
 }
 
 fetchData();
+/* market overview ends */
